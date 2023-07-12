127b5aa33aa92193ee732ef3af329b2ebf5b6dca
LSapee <dlworhks94@gmail.com>
2023-07-13 00:55:01 +0900

    6-8 카드 만들기까지 완료.

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ nomadcoders 강의 연습
 
 UI 만드는 법에 대해서
 
+CORS 문제 
+
+flutter 설정 폴더에 가서 flutter\bin\cache폴더에서 flutter_tools.stamp파일 삭제
+
+flutter\packages\flutter_tools\lib\src\web에서 chrome.dart 파일을 열고  '--disable-extensions' 의 아래에 '--disable-web-security' 를 추가하여 해결하였습니다.
+
+* 이후 크롬으로 디바이스 설정 후 실행하면 작동은 잘되나 지원되지 않는 명령줄 플래그 하면서 보안에 문제가 있을 수 있다는 문구가 나오네요.
+
+해당문제는 크롬웹을 디바이스로 설정했을때만 발생하였습니다.
--- lib/screens/home_screen_toon.dart
+++ lib/screens/home_screen_toon.dart
@@ -28,19 +28,65 @@ class HomeScreenToon extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.builder(
-              // 가로 스크롤 스크롤 될때마다 빌드되서 메모리 사용을 적게함
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                print(index);
-                var webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
+            return Column(
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Expanded(child: makeList(snapshot)),
+              ],
             );
           }
-          return Text("Loading...");
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      // 가로 스크롤 스크롤 될때마다 빌드되서 메모리 사용을 적게
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 6,
+                      offset: const Offset(10, 10),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]),
+              child: Image.network(
+                webtoon.thumb,
+                headers: const {
+                  "Access-Control-Allow-Origin": "*",
+                  "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+                  "Access-Control-Allow-Headers": "Content-Type",
+                  "User-Agent":
+                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(webtoon.title),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => const SizedBox(
+        width: 40,
       ),
     );
   }
